1464487bd5c60d4b67f671dee736a9cd3bb861f0
Fourfingerz <chansd5@gmail.com>
2022-08-30 17:27:48 -0400

    Added baseline auth content

---
+++ packages/auth/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+// import Landing from './components/Landing';
+// import Pricing from './components/Pricing';
+
+// Prevents class name collisions between auto-generated CSS class names 
+// ie: jss1 and jss1 from two microfrontends using the same material-ui StylesProvider
+const generateClassName = createGenerateClassName({
+	productionPrefix: 'au',  // css styles scoped for 'marketing', hence 'ma'
+});
+
+export default ({ history }) => {
+	return <div>
+		<StylesProvider generateClassName={generateClassName}>
+			<Router history={history}>
+				<Switch>
+				</Switch>
+			</Router>
+		</StylesProvider>
+	</div>
+};
