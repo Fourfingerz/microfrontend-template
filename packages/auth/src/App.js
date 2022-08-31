fcc033f8dce2f5bbcf737ea10b0fa10662ac58cf
Fourfingerz <chansd5@gmail.com>
2022-08-31 18:18:20 -0400

    Save progress

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-// import Landing from './components/Landing';
-// import Pricing from './components/Pricing';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 // Prevents class name collisions between auto-generated CSS class names 
 // ie: jss1 and jss1 from two microfrontends using the same material-ui StylesProvider
 const generateClassName = createGenerateClassName({
-	productionPrefix: 'au',  // css styles scoped for 'marketing', hence 'ma'
+	productionPrefix: 'au',  // css styles scoped for 'auth', hence 'au'
 });
 
 export default ({ history }) => {
@@ -16,6 +16,8 @@ export default ({ history }) => {
 		<StylesProvider generateClassName={generateClassName}>
 			<Router history={history}>
 				<Switch>
+					<Route path="/auth/signin" component={Signin} />
+					<Route path="/auth/signup" component={Signup} />
 				</Switch>
 			</Router>
 		</StylesProvider>
