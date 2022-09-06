507b08f4744a5b6a0e453a556ccaeb47b644f642
Fourfingerz <chansd5@gmail.com>
2022-09-06 17:42:24 -0400

    Try again x

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -25,7 +25,6 @@ const history = createBrowserHistory();
 
 // # dir
 
-
 export default () => {
   // This is a placeholder for ACTUAL user authentication
   // This is just a dummy using minimal React state
